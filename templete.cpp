22c2c82b286fa44b86fbe0597ed916b34c2ff815
Wsuika <wsuika.dev@gmail.com>
2021-09-11 20:43:31 +0800

    Update templete.cpp
    update template

--- templete.cpp
+++ templete.cpp
@@ -3,30 +3,35 @@
 #include <string>
 using namespace std;
 
+//solution should be here
+
+struct Testcase {
+    vector<int> candidates;
+    int target;
+    vector<vector<int> > answer;
+    
+    Testcase(vector<int> candidates,
+             int target,
+             vector<vector<int> > answer)
+    : candidates(candidates), target(target), answer(answer) {}
+};
+
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    vector<vector<int> > testcases =
+    // insert code here... candidates = [2,3,6,7], target = 7
+    vector<Testcase> testcases =
     {
-        {7,1,5,3,6,4},
-        {1,2,3,4,5},
-        {2,4,5,1000}
+       // Testcase({2,3,6,7},7,{{2,2,3},{7}}),
+       //Testcase({2,3,5}, 8, {{2,2,2,2},{2,3,3},{3,5}}),
+        Testcase({1,2,3,5}, 150, {{2,2,2,2},{2,3,3},{3,5}}),
     };
-    vector<int> answers =
-    {
-        7,
-        4,
-        998,
-    };
-    if(testcases.size()!=answers.size())
-    {
-        cout<<"test cases are not match answer!!!!"<<endl;
-    }
+
+
     Solution s;
     string str;
     for (int i=0; i<testcases.size(); i++) {
-        int ans = s.maxProfit(testcases[i]);
+        auto ans = s.combinationSum(testcases[i].candidates, testcases[i].target);
         
-        if(ans==answers[i])
+        if(ans==testcases[i].answer)
         {
             str = "👌 your ans:";
         }
@@ -35,7 +40,8 @@ int main(int argc, const char * argv[]) {
             str = "❌ your ans:";
             
         }
-        cout<<str<<ans<<endl;
+        cout<<str<<endl;
     }
     return 0;
 }
+
